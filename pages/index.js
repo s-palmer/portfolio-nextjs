53cf4fa1fc74c9e6a52e2b3f708e129965f5def7
Sergei Palmer <zigpalmer@gmail.com>
2021-12-17 10:14:04 +0000

    Added Github link

--- pages/index.js
+++ pages/index.js
@@ -56,14 +56,16 @@ export default function Home() {
       </main>
 
       <footer className={styles.footer}>
+
+        
         <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+          href="https://github.com/s-palmer"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Powered by{' '}
+          Find me on{' '}
           <span className={styles.logo}>
-            <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
+            <Image src="/gh-logo.png" alt="Github Logo" width={50} height={20.5} />
           </span>
         </a>
       </footer>
